9a2b5b337c9e907928db52186b778865e8e55a37
Gudmunduro <goh140400@gmail.com>
2019-02-06 14:32:34 +0000

    Project created

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'netforritun_mid'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
