92b93835798c775df70feff2e5d676a2379ff94f
Helgi Steinarr Júlíusson <helgiju327@nemi.tskoli.is>
2019-02-08 13:02:49 +0000

    homepage

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,31 +1,26 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Pictionary Project!
-        </span>
-        <system-information></system-information>
-      </div>
+    <header>
+		<span class="flex_item">
+			
+		</span>
+		<span class="flex_item flex_item_mid">
+			<h1>Pictionary!</h1>
+		</span>
+		<span class="flex_item">
+			<h2>Fisedush Ent.</h2>
+		</span>
+		
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+    </header>
+    <main>
+		<div class="left-side">
+			<span class="title">
+				Sum info
+			</span>
+			<system-information></system-information>
+		</div>
+   	</main>
   </div>
 </template>
 
@@ -44,85 +39,69 @@
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url('https://fonts.googleapis.com/css?family=Alice|Fredericka+the+Great|Righteous|ZCOOL+KuaiLe');
+/*
+	font-family: 'ZCOOL KuaiLe', cursive;               -Pictionary font
+	font-family: 'Fredericka the Great', cursive;       -Pictionary font   
+	font-family: 'Righteous', cursive;                  -FD Font
+	font-family: 'Alice', serif;                        -FD Font
+*/
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+body { font-family: 'Source Sans Pro', sans-serif; }
 
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
+#wrapper {
+	background:
+	radial-gradient(
+		ellipse at top left,
+		rgba(255, 255, 255, 1) 40%,
+		rgba(229, 229, 229, .9) 100%
+	);
+	height: 100vh;
+	padding: 60px 80px;
+	width: 100vw;
+}
+header{
+	display: flex;
+	justify-content: center;
+	width: 100%;
+}
+header span.flex_item{
+	font-family: 'Alice', serif;
+	width: 25%;
+	font-size: 18px;
+	text-align: center;
+}
+header span.flex_item_mid{
+	width: 50%;
+	font-family: 'ZCOOL KuaiLe', cursive;
+	font-size: 30px;
+}
+main {
+	display: flex;
+	justify-content: space-between;
+}
+main > div { flex-basis: 50%; }
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
+.left-side {
+	display: flex;
+	flex-direction: column;
+}
+.title {
+	color: #2c3e50;
+	font-size: 20px;
+	font-weight: bold;
+	margin-bottom: 6px;
+}
 
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
-  }
+.title.alt {
+	font-size: 18px;
+	margin-bottom: 10px;
+}
 </style>
